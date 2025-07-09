909923701cfae422c0a15f50bd10f17f10070db2
jeffrin172004 <your-username@users.noreply.github.com>
2025-07-09 19:08:49 +0530

    config added

---
+++ src/config.py
@@ -0,0 +1,9 @@
+EMAIL_CONFIG = {
+    "provider": "gmail",  # Set to "gmail" or "outlook"
+    "imap_server": "imap.gmail.com",  # Use "imap-mail.outlook.com" for Outlook
+    "username": "",  # Replace with your test email
+    "gmail_client_secret": "C:\\Users\\jeffr\\OneDrive\\Desktop\\email_clustering_project\\client_secret.json" # Path to Gmail OAuth client secret JSON
+    
+}
+#"outlook_client_id": "your_outlook_client_id",  # Replace with Azure app Client ID
+#"outlook_client_secret": "your_outlook_client_secret"  # Replace with Azure app Client Secret
